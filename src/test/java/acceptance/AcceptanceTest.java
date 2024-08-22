af04e26db44cf72547d32ee4e6a2b6e2f8f89843
MohammadSalemKhalaf <s12112969@stu.najah.edu>
2024-08-22 11:32:41 -0700

    Update AcceptanceTest.java

--- src/test/java/acceptance/AcceptanceTest.java
+++ src/test/java/acceptance/AcceptanceTest.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        features = "Features/user/ExplorationAndPurchase.feature",
+        features = "Features",
         plugin = {"html:target/cucumber-report/report.html"},
         monochrome = true,
         glue = "acceptance"
